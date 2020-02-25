b5ee3c1bcd9bcfb53e1bc9ca1a2bf82acdbfcd59
SAISRINIVASA LIKHIT KOTA <saisrinivasa.likhit@students.iiit.ac.in>
2020-02-26 01:28:02 +0530

    mod favicon

---
+++ src/Base.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Launcher} from 'react-chat-window';
+
+class Base extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            messageList: []
+        };
+    }
+
+    _onMessageWasSent(message) {
+        this.setState({
+            messageList: [...this.state.messageList, message]
+        })
+    }
+
+    _sendMessage(text) {
+        if(text.length > 0) {
+            this.setState({
+                messageList: [...this.state.messageList, {
+                    author: 'them',
+                    type: 'text',
+                    data: {text}
+                }]
+            })
+        }
+    }
+
+    render() {
+        return ( 
+            <div class="align-center">
+                <Launcher agentProfile={{
+                    teamName: 'GandhiAI',
+                    imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+                }}
+                onMessageWasSent={this._onMessageWasSent.bind(this)}
+                messageList={this.state.messageList}
+                showEmoji 
+                newMessagesCount
+                />
+            </div>
+        )
+    }
+}
+
+export default Base
